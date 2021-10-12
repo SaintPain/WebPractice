6fee455736343fcc2c70889be5abc2038f063a1b
201515045 <33309236+SaintPain@users.noreply.github.com>
2021-10-12 15:35:25 +0900

    Add files via upload

---
+++ js/details_slider.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){ 
+    $('.bxslider').bxSlider({ 
+        minSlides: 1,
+        maxSlides: 3,
+        moveSlides: 1,
+        slideWidth: 200,
+        slideMargin: 40,
+        prevText: '<i class="fas fa-chevron-left"></i>',
+        nextText: '<i class="fas fa-chevron-right"></i>',
+        autoControls: false,
+        controls: true,
+        pager: false,
+    }); 
+});
